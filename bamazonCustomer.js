777eba6fe3217e5950d99bc323b6fb34598e902b
KB Total Recall <kanwar@gmail.com>
2019-03-25 18:41:43 -0700

    Add logic to Quit when Q is enter for either Purchase or quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,15 +43,19 @@ function buyProduct(response) {
         {
             name: 'Item_id',
             type: 'Input',
-            message: "What is the ID of the item you would like to purchase"
+            message: "What is the ID of the item you would like to purchase[Quit with Q]"
         },
         {
             name: 'Quantity',
             type: 'Input',
-            message: "How many would you like to buy"
+            message: "How many would you like to buy[Quit with Q]"
         }
 
     ]).then(function (answer) {
+        if(answer.Item_id ==='Q' || answer.Quantity ==='Q'){
+            connection.end();
+            return;
+        }
         response.forEach(function(element){
             if(parseInt(element.item_id) === parseInt(answer.Item_id)){
                 if(element.stock_quantity >= parseInt(answer.Quantity)){
